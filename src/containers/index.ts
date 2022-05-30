37c62560a77b0248b2dc940fb2c4df05f4a5ec4b
Bryan Szendel <bryan@bigarmor.com>
2022-05-30 15:44:30 -0400

    remove more blog refs

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,4 @@
 export { default as About } from './about';
-export { default as BlogPostList } from './blog-post-list';
 export { default as Projects } from './projects';
 export { default as Welcome } from './welcome';
+
